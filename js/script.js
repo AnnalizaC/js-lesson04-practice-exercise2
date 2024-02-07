bc073fa8ec4ff7d776211059766b2687c3684421
Annaliza_C <aldcadiente@gmail.com>
2024-02-07 14:29:31 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Change the colors of the instagram and facebook icons
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+//Add a border to the opening hours paragraph
+var openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+
+//Add padding to opening hours paragraph
+openingHours.style.padding = "1em";
